7e903964e453053c9c33d5816f216093e5f7e422
maksim_ka <maksim_ka@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2010-06-25 12:16:05 -0500

    [fpBuildPlugin] test directories for the plugin
    git-svn-id: http://svn.symfony-project.com/plugins/fpBuildPlugin/trunk@29992 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ test/phpunit/fpBuildRootTestSuite.php
@@ -0,0 +1,15 @@
+<?php
+
+class fpBuildRootTestSuite extends sfBasePhpunitTestSuite
+{
+	/**
+	 * Dev hook for custom "setUp" stuff
+	 */
+	protected function _start()
+	{
+	}
+	
+	protected function _end()
+	{
+	}
+}
